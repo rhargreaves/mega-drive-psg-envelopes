d1f81fd418c1b8f0ce95dc491bd0dfdc5576b00b
rhargreaves <robert@roberthargreaves.net>
2019-12-20 23:45:25 +0000

    Produce tone

--- src/main.c
+++ src/main.c
@@ -3,6 +3,10 @@
 int main()
 {
     VDP_drawText("PSG Envelope Testing", 10, 1);
+
+    PSG_setFrequency(0, 440);
+    PSG_setEnvelope(0, PSG_ENVELOPE_MAX);
+
     while (TRUE) {
         VDP_waitVSync();
     }
