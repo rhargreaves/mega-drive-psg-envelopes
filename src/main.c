d8bb56031bb1a1cdd1cc3a0526a3cdee7315eed2
rhargreaves <robert@roberthargreaves.net>
2020-02-23 23:18:18 +0000

    Initialise Echo

--- src/main.c
+++ src/main.c
@@ -1,33 +1,23 @@
+#include "echo.h"
+
 #include <genesis.h>
-
-#define VIB_LENGTH 4
-
-static s16 freq = 440;
-static s16 vibDeltas[VIB_LENGTH] = { 0, 3, 0, -3 };
-static u16 curTick = 0;
-static u16 phase = 0;
 
 void tick(void)
 {
-    if (curTick == 0) {
-        PSG_setFrequency(0, freq + vibDeltas[phase]);
-
-        phase++;
-        if (phase == VIB_LENGTH) {
-            phase = 0;
-        }
-    }
-    curTick++;
-    if (curTick == 2) {
-        curTick = 0;
-    }
 }
 
 int main()
 {
     VDP_drawText("PSG Envelope Testing", 10, 1);
 
-    PSG_setEnvelope(0, PSG_ENVELOPE_MAX);
+    const u8 psgInstrument[] = { 0xFE, 0x00, 0xFF};
+
+    const void* const instrumentList[] = {
+      psgInstrument,
+      NULL
+    };
+
+    echo_init(instrumentList);
 
     while (TRUE) {
         VDP_waitVSync();
