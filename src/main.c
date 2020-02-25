586591055644cc34925fdeccc56d726da5756668
rhargreaves <robert@roberthargreaves.net>
2020-02-25 22:24:46 +0000

    Err, not quite right

--- src/main.c
+++ src/main.c
@@ -10,14 +10,19 @@ int main()
 {
     VDP_drawText("PSG Envelope Testing", 10, 1);
 
-    const u8 psgInstrument[] = { 0xFE, 0x00, 0xFF};
+    const u8 psgInstrument[] = { 0xFE, 0x00, 0xFF };
 
-    const void* const instrumentList[] = {
-      &psgInstrument,
-      NULL
-    };
+    const void* const instrumentList[] = { &psgInstrument, NULL };
 
     echo_init(instrumentList);
+
+    const u8 noteOnPsg1 = 0x08;
+    const u8 note = 0x50;
+    const u8 endEsf = 0xFF;
+
+    const u8 esf[] = { noteOnPsg1, note, endEsf };
+
+    echo_play_direct(&esf);
 
     while (TRUE) {
         VDP_waitVSync();
